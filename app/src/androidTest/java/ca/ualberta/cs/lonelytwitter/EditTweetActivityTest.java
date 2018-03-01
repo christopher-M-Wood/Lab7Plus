83f26c67589506f614480869c9db1e2cef875eae
christopher-M-Wood <cmwood@ualberta.ca>
2018-02-28 22:13:28 -0700

    Should be final tests

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/EditTweetActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/EditTweetActivityTest.java
@@ -2,6 +2,7 @@ package ca.ualberta.cs.lonelytwitter;
 
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
 
 import com.robotium.solo.Solo;
 
@@ -13,7 +14,7 @@ public class EditTweetActivityTest extends ActivityInstrumentationTestCase2 {
     private Solo solo;
 
     public EditTweetActivityTest() {
-        super(ca.ualberta.cs.lonelytwitter.EditTweetActivity.class);
+        super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
     }
     public void setUp() throws Exception{
         solo = new Solo(getInstrumentation(), getActivity());
@@ -21,5 +22,18 @@ public class EditTweetActivityTest extends ActivityInstrumentationTestCase2 {
     public void testStart() throws Exception {
         Activity activity = getActivity();
     }
-    
+    public void testEdit(){
+        solo.assertCurrentActivity("Wrong Activity", LonelyTwitterActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.body), "Test Tweet!");
+        solo.clickOnButton("Save");
+        solo.clearEditText((EditText) solo.getView(R.id.body));
+        assertTrue(solo.waitForText("Test Tweet!"));
+        solo.clickInList(0);
+        solo.assertCurrentActivity("Wrong Activity, no change", EditTweetActivity.class);
+        assertTrue(solo.waitForText("Test Tweet!"));
+    }
+    @Override
+    public void tearDown() throws Exception{
+        solo.finishOpenedActivities();
+    }
 }
